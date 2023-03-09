4b7108e57176c1e4cb05d38aad45fbe5a3fb6b35
The Void Block <72237088+Notch1792@users.noreply.github.com>
2023-03-09 15:35:22 -0800

    text-tools

--- random-string-generator/index.js
+++ random-string-generator/index.js
@@ -1,3 +1,16 @@
 function generate() {
-    console.log("test");
+    
+    let input = document.getElementById("input").value.split(document.getElementById("seperator").value);
+    let min = document.getElementById("min").value;
+    let max = document.getElementById("max").value;
+    let output = "";
+    
+    for(let i = 0; i < randomInt(min, max); i++) {
+        console.log(i);
+    }
+
+}
+
+function randomInt(min, max) {
+    return Math.random() * (max - min) + min;
 }
