96059d405b577cb171d4f350c4e046262867e340
The Void Block <72237088+Notch1792@users.noreply.github.com>
2023-03-25 17:45:02 -0700

    text-tools

--- ascii-alphabet-generator/index.js
+++ ascii-alphabet-generator/index.js
@@ -1,1 +1,20 @@
+function generate() {
 
+    let input = document.getElementById("input").value;
+    let output = "";
+
+    for(let x = 0; x < input.length; x++)
+        if(input[x] == '\\')
+            for(let i = 1; i <= 3; i++) {
+
+                let holdout = "";
+
+                if(input[x] != ',')
+                    holdout+= input[x + i];
+                else break;
+
+            }
+
+    document.getElementById("output").value = output;
+
+}
