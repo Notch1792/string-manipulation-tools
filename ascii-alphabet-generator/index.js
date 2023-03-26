3e4bbc998d024a3cf27c03a8a4a6a67fe16ec3f9
The Void Block <72237088+Notch1792@users.noreply.github.com>
2023-03-25 17:58:19 -0700

    text-tools

--- ascii-alphabet-generator/index.js
+++ ascii-alphabet-generator/index.js
@@ -6,19 +6,19 @@ function generate() {
     for(let x = 0; x < input.length; x++)
         if(input[x] == '\\') {
 
-            let first = "";
-            let second = "";
+            let first = 0;
+            let second = 0;
 
             for(let firstIndex = 1; firstIndex <= 3; firstIndex++) {
 
                 if(input[x] != ',')
-                    first+= input[x + firstIndex];
+                    first+= parseInt(input[x + firstIndex]);
                 else {
 
                     for(let secondIndex = 1; secondIndex <= 3; secondIndex++) {
 
                         if(input[x] != '\\' || input[x] != '\\')
-                            second+= input[x + firstIndex];
+                            second+= parseInt(input[x + firstIndex]);
 
                     }
 
